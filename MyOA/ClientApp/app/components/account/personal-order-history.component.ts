619b2b59df50796f11c3dc24452e9559da6d10be
terry <337247014@qq.com>
2018-03-30 16:27:42 +0800

    add all pages and router

---
+++ MyOA/ClientApp/app/components/account/personal-order-history.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'personal-order-history',
+    templateUrl: './personal-order-history.component.html',
+    styleUrls: ['../app/app.component.css']
+})
+export class PersonalOrderHistoryComponent {
+
+}
---
+++ MyOA/ClientApp/app/components/account/profile.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'user-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['../app/app.component.css']
+})
+export class ProfileComponent {
+
+}
